b17153e374d54510af513c6a53fb6610d51116c7
Mahmoud Shtayeh <mshtayeh.c@stc.com.sa>
2025-06-23 02:01:25 +0300

    Setup DBMS

--- src/main/java/com/gs1/ruleengine/RuleEngineApplication.java
+++ src/main/java/com/gs1/ruleengine/RuleEngineApplication.java
@@ -1,5 +1,7 @@
 package com.gs1.ruleengine;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,13 +11,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Mahmoud Shtayeh
  */
 @SpringBootApplication
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RuleEngineApplication {
     /**
      * Application entry point
      *
      * @param args Command line passed in arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(RuleEngineApplication.class, args);
     }
 }
